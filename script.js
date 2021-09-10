f5e40b555ac39cedf8420735ba38647fa285d210
govindgojiya <82605471+govindgojiya@users.noreply.github.com>
2021-09-10 22:15:12 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let leftHand = document.getElementById('hand-left');
+let rightHand = document.getElementById('hand-right');
+let pwdbox = document.getElementById('password-input');
+let emailbox = document.getElementById('email-input');
+let eyeRight = document.getElementById('eye-right');
+let eyeLeft = document.getElementById('eye-left');
+let i = 0;
+
+pwdbox.addEventListener('focusin', () => {
+    leftHand.classList.toggle('show-left');
+    leftHand.classList.toggle('show-left-anim');
+    rightHand.classList.toggle('show-right');
+    rightHand.classList.toggle('show-right-anim');
+})
+
+
+pwdbox.addEventListener('focusout', () => {
+    leftHand.classList.toggle('show-left');
+    leftHand.classList.toggle('show-left-anim');
+    rightHand.classList.toggle('show-right');
+    rightHand.classList.toggle('show-right-anim');
+})
+
+emailbox.addEventListener('keyup', (e) => {
+    let len = emailbox.value.length;
+    i = len;
+    i = i / 3 - 2;
+    eyeLeft.style.transform = `translateX(${i}%)`;
+    eyeRight.style.transform = `translateX(${i}%)`;
+})
